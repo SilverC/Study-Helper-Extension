2cba7234a4a33709d97fb5e053e34fab5f6adfff
Casey Silver <csilver@captechconsulting.com>
2016-02-21 16:16:15 -0500

    Initial commit

---
+++ background.js
@@ -0,0 +1,38 @@
+var isExtensionOn = true;
+var urls = ["facebook.com", "twitter.com", "instagram.com"];
+
+/*
+ * Intercepts browser request and redirects them to local page if they are blocked
+ */
+function interceptRequest(request)
+{
+    if(request && request.url && isExtensionOn)
+    {
+        if(request.type == "main_frame") // new page/site is loading in main window
+        {
+            for(var url of urls) {
+                if(request.url.indexOf(url) > -1)
+                {
+                    return {redirectUrl: chrome.extension.getURL("blocked.html")};
+                }  
+            }
+            
+        }
+    }
+}
+
+chrome.webRequest.onBeforeRequest.addListener(interceptRequest, {urls: ["<all_urls>"]}, ['blocking']);
+
+/*
+ * Handles messages for button states
+ */
+function buttonStateHandler(request, sender, sendResponse) {
+    if(request.cmd == "GetButtonState") {
+        sendResponse(isExtensionOn);
+    }
+    else if(request.cmd == "SetButtonState") {
+        isExtensionOn = request.data.value;
+    }
+}
+
+chrome.extension.onMessage.addListener(buttonStateHandler);
---
+++ popup.js
@@ -0,0 +1,26 @@
+function setButtonState(state) {
+    chrome.extension.sendMessage({ cmd: "SetButtonState", data: { value: state } });
+}
+
+function getButtonState() {
+    chrome.extension.sendMessage({ cmd: "GetButtonState" }, function (response) {
+        initializeButton(response);
+    });
+}
+
+function initializeButton(value) {
+    document.getElementById("myonoffswitch").checked = value;
+}
+
+function handleButtonChange(e) {
+    setButtonState(e.target.checked)
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+      document.getElementById("myonoffswitch").addEventListener('change', handleButtonChange);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+      getButtonState();
+      document.getElementById("onoffswitchlabel").className = "onoffswitch-label";
+});
