b39c1d2830dd5e784aceb07fc69c24575511e513
Casey Silver <csilver@captechconsulting.com>
2016-02-21 20:21:51 -0500

    Try string print

--- scripts/chrome-vsts-deploy.ps1
+++ scripts/chrome-vsts-deploy.ps1
@@ -1,4 +1,4 @@
 $source = ".\src\chrome"
-Write-Host $env:FILE_NAME
+Write-Host "environment file name variable: $env:FILE_NAME"
 Add-Type -A System.IO.Compression.FileSystem
 [IO.Compression.ZipFile]::CreateFromDirectory($source, $env:FILE_NAME)
