d16902b38b20beb45ae18f6c759e01ecfc36f593
Casey Silver <csilver@captechconsulting.com>
2016-02-21 20:11:54 -0500

    Try to print

--- scripts/chrome-vsts-deploy.ps1
+++ scripts/chrome-vsts-deploy.ps1
@@ -1,4 +1,4 @@
 $source = "src\chrome"
-$env:FILE_NAME
+Write-Host $env:FILE_NAME
 Add-Type -A System.IO.Compression.FileSystem
 [IO.Compression.ZipFile]::CreateFromDirectory($source, $env:FILE_NAME)
