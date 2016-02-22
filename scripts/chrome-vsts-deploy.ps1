7a66e08f74576caf75b7a1ac3e5cee8bc4cda337
Casey Silver <csilver@captechconsulting.com>
2016-02-21 20:18:20 -0500

    Current file location

--- scripts/chrome-vsts-deploy.ps1
+++ scripts/chrome-vsts-deploy.ps1
@@ -1,4 +1,4 @@
-$source = "src\chrome"
+$source = ".\src\chrome"
 Write-Host $env:FILE_NAME
 Add-Type -A System.IO.Compression.FileSystem
 [IO.Compression.ZipFile]::CreateFromDirectory($source, $env:FILE_NAME)
