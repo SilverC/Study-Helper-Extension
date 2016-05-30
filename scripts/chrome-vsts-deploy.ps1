948604898593eb5e3e29318d2caf893d2d81e9c2
Casey Silver <csilver@captechconsulting.com>
2016-05-30 17:24:17 -0400

    Remove commas

--- scripts/chrome-vsts-deploy.ps1
+++ scripts/chrome-vsts-deploy.ps1
@@ -1,12 +1,12 @@
 $source = ".\src\chrome"
-Write-Host "environment file name variable: $env:FILE_NAME"
+Write-Host "environment file name variable: $($env:FILE_NAME)"
 Add-Type -A System.IO.Compression.FileSystem
 [IO.Compression.ZipFile]::CreateFromDirectory($source, $env:FILE_NAME)
 
 $body = @{
-    refresh_token = $env:REFRESH_TOKEN,
-    client_id = $env:CLIENT_ID,
-    client_secret = $env:CLIENT_SECRET,
+    refresh_token = $env:REFRESH_TOKEN
+    client_id = $env:CLIENT_ID
+    client_secret = $env:CLIENT_SECRET
     grant_type = "refresh_token"
 }
 Write-Output $body
